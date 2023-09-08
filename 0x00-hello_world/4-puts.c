eefeaeed8712e63af29677f3f4e985d0579f08d0
ShakoKal <rusteau@iss.sc>
2023-09-08 19:59:14 +0000

    c Exercises01

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,6 @@
 #include <stdio.h>
-
-/**
- * main - Entry point of the program
- */
 int main(void)
 {
-	/* Uses the puts function to print the quote to the standard output */
-	puts("\"Programming is like building a multilingual puzzle");
-
-	return (0);
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
 }
