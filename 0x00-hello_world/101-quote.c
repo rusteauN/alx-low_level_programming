aad0268156120c9493586e4c71fff2a12274f030
ShakoKal <rusteau@iss.sc>
2023-09-08 19:50:04 +0000

    c Exercises01

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+
+/**
+ * main - A program prints a specific quote to the standard error.
+ *
+ * Description:	This C program prints the quote
+ *		"and that piece of art is useful\" - Dora Korpar, 2015-10-19"
+ *              followed by a new line, to the standard error
+ *              using the write system call.
+ *
+ * Return: Always 1 (Error).
+ *
+ * 
+ * 
+ */
+int main(void)
+{
+	/* Use the write system call to print the quote to the standard error.*/
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+	return (1);
+}
---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ */
+int main(void)
+{
+	/* Uses the puts function to print the quote to the standard output */
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
