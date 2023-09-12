877d871db9e8c7283d45887c13bd39911bd4c7e6
ShakoKal <rusteau@iss.sc>
2023-09-12 18:28:10 +0000

    c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Print "_putchar" followed by a new line.
+ */
+int main(void)
+{
+	char putchar[] = "_putchar";
+	int i = 0;
+
+	while (putchar[i] != '\0')
+	{
+		_putchar(putchar[i]);
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
