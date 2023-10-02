72c5cc279e3d67b429a77eb6319022adada40e4a
ShakoKal <rusteau@iss.sc>
2023-10-02 19:15:19 +0000

    arg

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program
+ * @argc: size of array command line arg.
+ * @argv: command line argument
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
